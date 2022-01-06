f38f8646db4407355478d8bb5ad45665b6c411a7
WaterKing <zhangsifanbj@163.com>
2022-01-06 23:51:30 +0800

    增加五子棋逻辑

--- UI/main.py
+++ UI/main.py
@@ -12,39 +12,61 @@ class App(tk.Tk):
         super().__init__()
         self.title(name)
         self.geometry("1024x768")
-        self.init()
+
+
+        self.worker = None
+        self.label = None
 
         self.count = None
         self.img = None
         self.photo = None
+
+        self.init()
 
     def init(self):
         '''
             初始化原界面
         :return: 
         '''
+        self.create_worker()
 
-        self.worker = tk.Frame(self, height= 600, width = 1024,bg = 'blue')
-        self.worker.pack()
+        # 设置每个格子的大小
+        for col in range(9):
+            self.button_frame.grid_columnconfigure(col, minsize = 30)
+        self.button_frame.grid_rowconfigure(0, minsize=40)
 
-        self.button1 = tk.Button(self, text ="倒计时", font = ('黑体', 15), bd = 3, width = 10, height=2, padx= 1, pady= 1, command=self.cdStart)
-        self.button1.place(relx = 0.15, rely = 0.85)
+        self.button1 = tk.Button(self.button_frame, font = ('黑体', 15), height=2, text = "倒计时", command = self.cdStart)
+        self.button1.grid(row=1, column=1)
 
-        self.button2 = tk.Button(self, text="人生模拟器", font = ('黑体', 15), bd = 3, width = 10, height=2, padx= 1, pady= 1, command=self.randomStart)
-        self.button2.place(relx= 0.35, rely = 0.85)
+        self.button2 = tk.Button(self.button_frame, font=('黑体', 15), height=2, text="倒计时")
+        self.button2.grid(row=1, column=3)
 
-        self.button3 = tk.Button(self, text="相册", font = ('黑体', 15), bd=3, width=10, height=2, padx=1, pady=1, command=self.showPhoto)
-        self.button3.place(relx= 0.55, rely = 0.85)
+        self.button3 = tk.Button(self.button_frame, font=('黑体', 15), height=2, text="倒计时")
+        self.button3.grid(row=1, column=5)
 
-        self.button4 = tk.Button(self, text="重置", font = ('黑体', 15), bd=3, width=10, height=2, padx=1, pady=1, bg = 'purple', command=self.reset)
-        self.button4.place(relx= 0.75, rely = 0.85)
+        self.button4 = tk.Button(self.button_frame, font=('黑体', 15), height=2, text="倒计时")
+        self.button4.grid(row=1, column=7)
+
+    def create_worker(self):
+        if self.worker is not None:
+            self.worker.destroy()
+        if self.label is not None:
+            self.label.destroy()
+
+        self.update()
+
+        self.worker = tk.Frame(self, height=self.winfo_height() * 0.8, width=self.winfo_width(), bg='blue')
+        self.worker.grid(row=0, column=0)
+
+        self.button_frame = tk.Frame(self, height=self.winfo_height() * 0.2, width=self.winfo_width())
+        self.button_frame.grid(row=1, column=0)
+
 
     def cdStart(self):
-        for windget in self.worker.winfo_children():
-            windget.destroy()
+        self.init()
 
         self.label = tk.Label(self.worker, text="倒计时开始！", font=("黑体", 40))
-        self.label.place(relx=0.35, rely=0.45)
+        self.label.place(relx=0.5, rely=0.5, anchor='center')
 
         self.count = askinteger("倒计时", "倒计时")
         self.countdown(self.count)
@@ -56,10 +78,10 @@ class App(tk.Tk):
         if self.remaining <= 0:
             self.label.configure(text="计时结束")
         else:
-            self.label.place(relx=0.45, rely=0.45)
             self.label.configure(text="%d" % self.remaining)
             self.remaining = self.remaining - 1
             self.after(1000, self.countdown)
+
 
     def randomStart(self):
         for windget in self.worker.winfo_children():
---
+++ gobang/main.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+# @Time     :2022/1/6 22:24
+# @Author   :Z
+# @File     :main.py
+import pygame
+
+_BLACK = 1
+_BLANK = 0
+_WHITE = -1
+
+class gobang():
+    def __init__(self):
+        # 棋盘与棋子的大小
+        self.cell_size = 60
+        self.cell_num = 15
+        self.space_size = 80
+        self.grid_length = self.cell_size * (self.cell_num - 1) + self.space_size * 2
+
+        # 设置棋子在后台的位置，初始全部为空
+        self.chess_pos = []
+        self.initChessBoard()
+        # 设置AI
+        self.use_AI = False
+        self.AI_turn = _WHITE
+
+    def initChessBoard(self):
+        for i in range(self.cell_num):
+            self.chess_pos.append([_BLANK] * self.cell_num)
+
+    def warning(self, chess):
+        # print("这个位置已经有{}棋了".format("黑" if chess == _BLACK else "白"))
+        pass
+
+    def judgeWin(self, x_i, y_i):
+        dir = [
+            [[-1, 0], [1, 0]],
+            [[0, -1], [0, 1]],
+            [[-1, -1], [1, 1]],
+            [[1, -1], [-1, 1]]
+        ]
+        cur_x_i = x_i
+        cur_y_i = y_i
+
+        for i in range(4):
+            count = 1
+            for j in range(2):
+                # 某一个方向查找
+                while True:
+                    cur_x_i += dir[i][j][0]
+                    cur_y_i += dir[i][j][1]
+                    # 处于界内
+                    if 0 <= cur_x_i < self.cell_num and 0 <= cur_y_i < self.cell_num:
+                        # 颜色相同
+                        if self.chess_pos[cur_x_i][cur_y_i] == self.turn:
+                            count += 1
+                            continue
+                        # 相对应的下一个方向
+                        break
+
+                cur_x_i = x_i
+                cur_y_i = y_i
+
+            if count >= 5:
+                return True
+
+        return False
+
+
+
+    def run(self):
+        self.screen = pygame.display.set_mode((self.grid_length, self.grid_length))
+        self.turn = _BLACK
+        while True:
+
+            if self.use_AI == True and self.turn == self.AI_turn:
+                # AI落子
+                pass
+            else:
+                # 人类落子
+                for event in pygame.event.get():
+                    # 如果是退出事件
+                    if event.type == pygame.QUIT:
+                        pygame.quit()
+                        exit()
+                    # 如果是鼠标单击事件
+                    if event.type == pygame.MOUSEBUTTONUP:
+                        x_pos, y_pos = pygame.mouse.get_pos()
+                        print(x_pos, y_pos)
+                        x_i = int(round((x_pos - self.space_size) / self.cell_size))
+                        y_i = int(round((y_pos - self.space_size) / self.cell_size))
+                        print(x_i, y_i)
+                        if 0 <= x_i < self.cell_num and 0 <= y_i < self.cell_num:
+                            # 如果是个空位置
+                            if self.chess_pos[x_i][y_i] == _BLANK:
+                                self.chess_pos[x_i][y_i] = self.turn
+                                # 落子完成后，要判断是否已经胜了
+                                if self.judgeWin(x_i, y_i):
+                                    print("win")
+
+                                # 换边
+                                self.turn = -self.turn
+
+                            else:
+                                self.warning(self.chess_pos[x_i][y_i])
+
+            # 显示界面为淡蓝色，棋盘线为白色
+            self.screen.fill((0, 121, 121))
+            for x in range(0, self.cell_num * self.cell_size, self.cell_size):
+                pygame.draw.line(
+                    self.screen,
+                    (200, 200, 200),
+                    (x + self.space_size, 0 + self.space_size),
+                    (x + self.space_size, self.cell_size * (self.cell_num - 1) + self.space_size),
+                    1
+                )
+
+            for y in range(0, self.cell_num * self.cell_size, self.cell_size):
+                pygame.draw.line(
+                    self.screen,
+                    (200, 200, 200),
+                    (0 + self.space_size, y + self.space_size),
+                    (self.cell_size * (self.cell_num - 1) + self.space_size, y + self.space_size),
+                    1
+                )
+
+            # 画棋子的位置
+            for x in range(self.cell_num):
+                for y in range(self.cell_num):
+                    color = None
+                    if(self.chess_pos[x][y] == _BLANK):
+                        continue
+                    elif self.chess_pos[x][y] == _WHITE:
+                        color = (205, 205, 205)
+                    else:
+                        color = (30, 30, 30)
+
+                    x_pos = self.space_size + x * self.cell_size
+                    y_pos = self.space_size + y * self.cell_size
+                    pygame.draw.circle(self.screen, color, (x_pos, y_pos), 24, 24)
+
+            pygame.display.update()
+
+if __name__ == "__main__":
+    game = gobang()
+    game.run()
